8d403518c6b6a4e829ae2a4ebf7b499cb53c12c5
Schwenn, Andrew (sx7625) <sx7625@att.com>
2021-04-09 11:30:42 -0500

    updated script from nick

--- feeder_server.py
+++ feeder_server.py
@@ -1,36 +1,37 @@
 from flask import Flask
+from flask import request
+from flask import abort
+
 import serial
 import time
 
 app = Flask(__name__)
 
-@app.route('/feed/<float:food_wt_lbs>')
-def send_feed(food_wt_lbs):
-    #time_run = time.time() + food_wt_lbs*5.0 + 2 #Use Calibration Equation Here or on Arduino
-    time_run = food_wt_lbs
-    ser = serial.Serial('/dev/ttyS0', 115200, timeout=5, parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS)
-    time.sleep(1)
+@app.route('/feed/<int:feeder_id>', methods=['POST'])
+def send_feed(feeder_id):
+    #run_time = time.time() + food_wt_lbs*5.0 + 2 #Use Calibration Equation Here or on Arduino
+    
+    run_time = request.form['runtime']
+    
+    #Replace with Format strings
+    print("Starting feed for "+str(run_time)+" seconds on feeder "+str(feeder_id)+".")
 
-    ser.write(str.encode("AT+ADDRESS=25\r\n"))
-    serial_payload = (ser.readline())[:-2]
-    print("Address set?", serial_payload.decode(encoding="utf-8"))
-    time.sleep(1)
-    ser.write(str.encode("AT+NETWORKID=6\r\n"))
-#     ser.write(str.encode("AT+BAND=915000000"))
-#     ser.write(str.encode("AT+ADDRESS="))
-    time.sleep(1)
-    ser.write(str.encode("AT?\r\n"))
-    serial_payload = (ser.readline())[:-2]
-    print("Module responding?", serial_payload.decode(encoding="utf-8"))
-    time_loop = time.time() + 5.0 #transmission time
-    while(1):# time.time() <= time_loop:
-        msg = "AT+SEND=116, " + str(len(str(time_run)))+", " +str(time_run)+ "\n"
-        ser.write(str.encode("AT+SEND=0, 4, hiii\r\n"))
-        print(msg)
-        
-        # line = ser.readline().decode('utf-8')
-        #print(line)
-    return('Feed'+str(food_wt_lbs))
+    try:
+        ser = serial.Serial('/dev/ttyS0', 115200, timeout=5, parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS)
+        time.sleep(1)
 
+        ser.write(str.encode("AT?\r\n"))
+        serial_payload = (ser.readline())[:-2]
+        print("Module responding?", serial_payload.decode(encoding="utf-8"))
+        time_loop = time.time() + 5.0 #transmission time or ACK needed
+        while time.time() <= time_loop:
+            #Or replace with AT+SEND=feeder_id
+            msg = "AT+SEND=20, " + str(len(str(run_time)))+", " +str(run_time)+ "\n"
+            ser.write(str.encode(msg))
+    except serial.serialutil.SerialException:
+        abort(500)
+
+    #Replace with Format strings
+    return('Success! Fed for '+str(run_time)+" seconds on feeder "+str(feeder_id)+".")

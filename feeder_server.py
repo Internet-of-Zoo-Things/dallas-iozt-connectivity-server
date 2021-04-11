6a21513b0e48a3ab6f30827ed307f8d2c213b5bf
kr0wl <fcorral@smu.edu>
2021-04-11 11:45:35 -0500

    Update feeder_server.py

--- feeder_server.py
+++ feeder_server.py
@@ -12,7 +12,7 @@ def send_feed(feeder_id):
     #run_time = time.time() + food_wt_lbs*5.0 + 2 #Use Calibration Equation Here or on Arduino
 
     run_time = request.json['runtime']
-    
+
     #Replace with Format strings
     print("Starting feed for "+str(run_time)+" seconds on feeder "+str(feeder_id)+".")
 
@@ -21,15 +21,30 @@ def send_feed(feeder_id):
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS)
         time.sleep(1)
+        ser.write(str.encode("AT+ADDRESS=10\r\n"))
+        time.sleep(1)
+        ser.write(str.encode("AT+NETWORKID=5\r\n"))
+        time.sleep(1)
+        ser.write(str.encode("AT+MODE=1\r\n"))
+        time.sleep(1)
+        ser.write(str.encode("AT+BAND=915000000\r\n"))
+        time.sleep(1)
+        ser.write(str.encode("AT+PARAMETER=10,7,1,7\r\n"))
+        time.sleep(5)
 
-        ser.write(str.encode("AT?\r\n"))
-        serial_payload = (ser.readline())[:-2]
-        print("Module responding?", serial_payload.decode(encoding="utf-8"))
-        time_loop = time.time() + 5.0 #transmission time or ACK needed
+        msg = "AT+SEND=20, " + str(len(str(run_time)))+", " +str(run_time)+ "\n"
+        count = 15
+        time_loop = time.time() + 15.0 #transmission time or ACK needed
         while time.time() <= time_loop:
-            #Or replace with AT+SEND=feeder_id
-            msg = "AT+SEND=20, " + str(len(str(run_time)))+", " +str(run_time)+ "\n"
             ser.write(str.encode(msg))
+            time.sleep(1)
+            line = ser.readline()
+            if len(line) == 28:
+               print("Communication Received: " + line.decode('utf-8'))
+               print("Ending Feed - Starting 30 sec  buffer")
+               time.sleep(30)
+               break
+            line=''
     except serial.serialutil.SerialException:
         abort(500)
 
